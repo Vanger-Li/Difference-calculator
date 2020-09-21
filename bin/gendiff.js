d5c755007b723d42b60b0ae64c452e9421afe75b
vanger-li <vitaliy.vanger32@gmail.com>
2020-09-21 12:32:27 +0500

    modify gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,8 @@
 import program from 'commander';
 
 program
-  .version('1.0.0', '-V', '--version', 'output the version number')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
