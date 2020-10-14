63ab17a416ab77e1c76db458fd87b08681b15c89
vanger-li <vitaliy.vanger32@gmail.com>
2020-10-14 18:55:49 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
-const genDiff = (path1, path2) => {
-    const pathToFile1 = path.resolve(process.cwd(), path1);
-    const pathToFile2 = path.resolve(process.cwd(), path2);
+const genDiff = (pathToFile1, pathToFile2) => {
+    const file1 = path.resolve(process.cwd(), pathToFile1);
+    const file2 = path.resolve(process.cwd(), pathToFile2);
 
-    const dataFromFile1 = JSON.parse(fs.readFileSync(pathToFile1, 'utf8'));
-    const dataFromFile2 = JSON.parse(fs.readFileSync(pathToFile2, 'utf8'));
+    const dataFromFile1 = JSON.parse(fs.readFileSync(file1, 'utf8'));
+    const dataFromFile2 = JSON.parse(fs.readFileSync(file2, 'utf8'));
 
     const keysOfFile1 = Object.keys(dataFromFile1);
     const keysOfFile2 = Object.keys(dataFromFile2);
