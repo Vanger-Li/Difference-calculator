9f6505e4f430558b1407d37b18ef5c2d0b3750cf
vanger-li <vitaliy.vanger32@gmail.com>
2020-10-25 19:29:17 +0500

    add test

---
+++ __test__/index.test.js
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+
+const expected = `{
+    - follow: false
+      host: hexlet.io
+    - proxy: 123.234.53.22
+    - timeout: 50
+    + timeout: 20
+    + verbose: true
+  }`;
+
+test('genDiff', () => expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expected));
